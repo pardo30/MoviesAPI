53399a7515d1b0d63c61a960ca4cfa9af5eef65c
Pardo30 <info@pardo30.com>
2021-03-28 18:30:34 +0200

    Vote Average ok

---
+++ src/components/Average.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Average.scss';
+
+const Average = (props) => {
+    const color = () => {
+        const average = props.average;
+        if (average >= 9) {
+            return "#00B24F"
+        } else if (average < 9 && average >= 8) {
+            return "#92D04F"
+        } else if (average < 8 && average >= 6) {
+            return "#FFFE01"
+        } else if (average < 6 && average >= 5) {
+            return "FEC000"
+        } else if (average < 5  && average >=3) {
+            return "FEC000"
+        } else {
+            return "FE0001"
+        }
+    }  
+    const styles = {
+        borderStyle: 'solid',
+        borderWidth: '5px',
+        borderColor: color(),
+    }
+    return (
+        <div className='average' style={styles}>
+            <p>{props.average}</p>
+        </div>
+    )
+}
+
+export default Average
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,29 +1,15 @@
 import React from 'react';
+import Average from './Average';
 import './Movie.scss';
 
 const Movie = ({movie}) => {
-  // const color = () => {
-  //   const average = {movie.vote_average};
-  //   if (average >= 9) {
-  //     return "#00B24F"
-  //   } elseif (average < 9 && average >= 8) {
-  //     return "#92D04F"
-  //   } elseif (average < 8 && average >= 6) {
-  //     return "#FFFE01"
-  //   } elseif (average < 6 && average >= 5) {
-  //     return "FEC000"
-  //   }elseif (average < 5  && average >=3) {
-  //     return "FEC000"
-  //   } else {
-  //     return "FE0001"
-  //   }
-  // }  
+  
   return (
         <div className="movie">
           <img src={"https://image.tmdb.org/t/p/w185"+movie.poster_path} alt="Movie poster"/>
           <h5>{movie.title}</h5>
-          <div className='average' style={{border: `5px solid white`}}>
-            <p>{movie.vote_average}</p>
+          <div className='average'>
+            <Average average={movie.vote_average}/>
           </div>
         </div>
     )
