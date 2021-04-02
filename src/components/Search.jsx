aad74c74ebbe956cc6491567620fce8a299655f6
Pardo30 <info@pardo30.com>
2021-04-02 12:15:09 +0200

    Search Ok

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,39 +4,37 @@ import Movie from './Movie';
 import './Search.scss';
 
 const Search = () => {
-    const [searchData, setSearchData] = useState('')
+    const [query, setQuery] = useState('')
     const [movies, setMovies] = useState([])
-    const data = '';
+    const [search, setSearch] = useState('')
     
-     
-    const handleSubmit = e => {
-        e.preventDefault();
-        }
-        
+    const press = (e) =>{
+        if (e.keyCode === 13 && !e.shiftKey) {
+            e.preventDefault();
+        }}
+
     useEffect( () => {
         const respose = async () => {
-            await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=6db8b1cae4de7ed79b6af06f87c63d15&language=es-ES&query=${data}`)
+            const result = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=6db8b1cae4de7ed79b6af06f87c63d15&language=es-ES&query=${search}`)
             .then(res=> setMovies(res.data.results))
             .catch(error => console.error(error))
         };
-        respose()
-        }, []);
+        respose();
+        }, [search]);
 
     return (
         <div>
            <div className="search" >
-               <form onSubmit={handleSubmit}>
-                   <input 
+                <input 
                    type='search'
                    autoFocus
                    placeholder='Busca una pelicula'
-                   onChange={e => setSearchData(e.target.value)}
+                   onChange={e => setQuery(e.target.value)}
+                   onKeyPress={press}
+                   value={query}
                     />
-                   <button type='submit'>Buscar</button>
-               </form>
+                   <button id='button' type='button' onClick={() => setSearch(query)}>Buscar</button>
            </div>
-           <div>Esta es la busqueda: {searchData}</div>
-
            <div className="result">
                {movies?.map(movie=><Movie movie={movie} key={movie.id}/>)}   
            </div>
