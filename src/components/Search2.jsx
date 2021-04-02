b13040d32e40716f10947d7c5e46e08c2afcf889
Pardo30 <info@pardo30.com>
2021-04-02 11:45:30 +0200

    Search bug

--- src/components/Search2.jsx
+++ src/components/Search2.jsx
@@ -6,16 +6,22 @@ import './Search.scss';
 const Search = () => {
     const [searchData, setSearchData] = useState('')
     const [movies, setMovies] = useState([])
-    const data = '';
     
-     
-    const handleSubmit = e => {
+    const handleChangeInput = e => {
+        const base = e.target;
         e.preventDefault();
+        setSearchData(base.value)
+    }
+    
+    const SearchMovie = e => {
+        e.stopPropagation();
+        console.log('solicitando')
+        e.currentTarget.reset();
         }
-        
-    useEffect( () => {
+
+    useEffect( (query = searchData) => {
         const respose = async () => {
-            await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=6db8b1cae4de7ed79b6af06f87c63d15&language=es-ES&query=${data}`)
+            await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=6db8b1cae4de7ed79b6af06f87c63d15&language=es-ES&query=${searchData}`)
             .then(res=> setMovies(res.data.results))
             .catch(error => console.error(error))
         };
@@ -24,14 +30,14 @@ const Search = () => {
 
     return (
         <div>
-           <div className="search" >
-               <form onSubmit={handleSubmit}>
+           <div className="search" onSubmit={handleSubmit}>
+               <form action="">
                    <input 
                    type='search'
                    autoFocus
                    placeholder='Busca una pelicula'
-                   onChange={e => setSearchData(e.target.value)}
-                    />
+                   onChange={onSearchChange}
+                   value={searchData}/>
                    <button type='submit'>Buscar</button>
                </form>
            </div>
