1edf678230d258a320fe730bdd92472c399e2692
Pardo30 <info@pardo30.com>
2021-04-04 12:53:13 +0200

    Initial Popup

--- src/components/MovieFile.jsx
+++ src/components/MovieFile.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Average from './Average'
 
-const MovieFile = () => {
+const MovieFile = ({movie}) => {
     return (
-        <div>
-            
+        <div className="popup">
+            <button className="closeButton" onClick={showPopup}> <<< </button>
+            <div className="box1">
+                <div className="poster">
+                    <img src={"https://image.tmdb.org/t/p/w185"+movie.poster_path} alt="Movie poster"/>
+                </div>
+                <div className="info">
+                    <Average average={movie.vote_average}/>
+                    {movie-adult}
+                    <h6 className='year'>{movie.release_date.substr(0,4)}</h6>
+                    <h5 className='title'>{movie.title}</h5>
+                </div>
+            </div>
+            <div className="box2">
+            <h6>Presupuesto: {movie.budget} $</h6>
+            <h6>Géneros:</h6>
+                {movie.generes.name}
+            <h6>Descripción general:</h6>
+                {movie.overview}
+            </div>
         </div>
     )
 }
--- src/container/Movies/Movies.jsx
+++ src/container/Movies/Movies.jsx
@@ -7,6 +7,7 @@ import MovieTitle from '../../components/MovieTitle';
 const Movies = props => {
     const movieType = props.match.params.movieType;
     const [movies, setMovies] = useState([]);
+    const [showPopup, setShowPopup] = useState(false)
 
     useEffect(() => {
         axios.get(`https://api.themoviedb.org/3/movie/${movieType}?api_key=6db8b1cae4de7ed79b6af06f87c63d15&&language=es-ES&include_image_language=en,null&page=1`)
@@ -22,7 +23,7 @@ const Movies = props => {
                 <MovieTitle movieType={movieType} />
             </div>
             <div className='movies'>
-                {movies?.map(movie=><Movie movie={movie} key={movie.id}/>)}
+                {movies?.map(movie=><Movie movie={movie} key={movie.id} showPopup={()=>setShowPopup(!showPopup)}/>)}
             </div>
         </div>
     )
